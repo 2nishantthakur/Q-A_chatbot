97dd22b0bf15fbb88ad8bfd0497f2917d5511c3c
2nishantthakur <2nishantthakur@gmail.com>
2024-07-27 10:14:13 +0530

    login added

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-import os
-import shutil
+'''
+latest version(25/08/24)
+user sessions working(not interfaring with different users)
+headings are working fine now
+'''
 import streamlit as st
 from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
+import os
+import shutil
 from get_embedding_function import get_embedding_function
 import populate_database
-from chromadb.config import Settings
+from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+import base64
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "PDFs"
-MODEL_NAME = "llama3"
+model_name = "llama3"
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
 
@@ -21,106 +30,151 @@ Answer the question based only on the following context:
 Answer the question based on the above context: {question}
 """
 
-def query_rag(query_text: str):
-    embedding_function = get_embedding_function()
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function, client_settings=Settings())
-
+def query_rag(query_text: str, username):
+    # embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH+"_"+username, embedding_function=get_embedding_function())
+    db.persist()
     results = db.similarity_search_with_score(query_text, k=5)
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-
-    model = Ollama(model=MODEL_NAME)
+    model = Ollama(model=model_name)
     response_text = model.invoke(prompt)
-
     return response_text
 
-def handle_pdf_upload(uploaded_file):
-    if os.path.exists(DATA_PATH):
-        shutil.rmtree(DATA_PATH)
-    os.makedirs(DATA_PATH)
-
-    save_path = os.path.join(DATA_PATH, uploaded_file.name)
+def handle_pdf_upload(uploaded_file, username, reset = True):
+    print("\n\nHandlePDF Called!\n\n")
+    if os.path.exists(DATA_PATH+"/"+username):
+        shutil.rmtree(DATA_PATH+"/"+username)
+    os.makedirs(DATA_PATH+"/"+username)
+    save_path = os.path.join(DATA_PATH+"/"+username, uploaded_file.name)
     with open(save_path, 'wb') as f:
         f.write(uploaded_file.getbuffer())
     st.session_state['pdf_uploaded'] = True
-    st.success(f"File saved to {save_path}")
+    st.sidebar.success(f"File saved to {save_path}")
+    
+    populate_database.main(reset, username = username)
 
-    populate_database.main(reset=True)
+def get_base64_of_image(image_file):
+    with open(image_file, "rb") as image:
+        return base64.b64encode(image.read()).decode()
 
 def main():
     st.set_page_config(page_title="Q&A with LLM", page_icon="💬", layout="wide")
 
-    st.markdown(
-        """
-        <style>
-            .main {
-                max-width: 100%;
-                overflow-x: auto;
-            }
-            .content {
-                display: flex;
-                flex-direction: column;
-                width: 2000px;  /* Adjust as needed to exceed viewport width */
-            }
-            .stTextArea, .stButton {
-                text-align: center;
-                margin: auto;
-            }
-            .highlight {
-                font-weight: bold;
-                font-size: 1.1em;
-                color: #4CAF50;
-            }
-            .file-uploader {
-                text-align: center;
-            }
-            /* Optional: Ensure that scrollbars are visible if necessary */
-            body {
-                overflow-x: auto;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
+    with open('config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+
+
+
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
     )
 
-    st.title("Q&A 💬")
-    st.markdown("""
-        <div style="text-align: center;">
-            This application allows you to ask questions to a language model. 
-            Enter your query below and click 'Submit' to get an answer based on the context.
-        </div>
-    """, unsafe_allow_html=True)
+    name, authentication_status, username = authenticator.login()
 
-    with st.container():
-        st.markdown("<div class='content'>", unsafe_allow_html=True)
+    # if authentication_status:
+    #     authenticator.logout('Logout', 'main')
+    #     st.write(f'Welcome *{name}*')
+    #     st.title('Some content')
+    # elif authentication_status == False:
+    #     st.error('Username/password is incorrect')
+    # elif authentication_status == None:
+    #     st.warning('Please enter your username and password')
 
-        st.markdown("<p class='highlight'>Add PDF</p>", unsafe_allow_html=True)
-        uploaded_file = st.file_uploader("", type="pdf")
+    if st.session_state["authentication_status"]:
+        
+        st.markdown("""
+        <style>
+            .title-container {
+                display: flex;
+                position: fixed;
+                top: 0;
+                margin-top: 40px;
+                align-items: center;
+                background: white;
+                padding: 0px;
+                width :100%;
+                border-bottom: 1px solid #e6e6e6;
+            }
+            .text-container {
+                flex: 1;
+                text-align: left;
+                margin-left: 10px;
+                
+            }
+            @media (prefers-color-scheme: dark) {
+                .title-container {
+                    background: transparent;
+                    color: white;
+                    border-bottom: 1px solid #555555;
+                }
+            }
+        </style>
+        """, 
+        unsafe_allow_html=True)
 
+        st.sidebar.title("Upload PDF")
+        uploaded_file = st.sidebar.file_uploader("", type="pdf")
+        
         if uploaded_file is not None:
             if "fileUpload" not in st.session_state:
-                st.session_state.fileUpload = uploaded_file
-                handle_pdf_upload(uploaded_file)
+                # st.session_state.fileUpload = uploaded_file
+                st.session_state.fileUpload = uploaded_file.name
+                print("first file fo session uploaded")
+                handle_pdf_upload(uploaded_file, username, reset = True)
             else:
-                if st.session_state.fileUpload != uploaded_file:
-                    st.session_state.fileUpload = uploaded_file
-                    handle_pdf_upload(uploaded_file)
+                if st.session_state.fileUpload!=uploaded_file.name:
+                    print("new file uploaded ")
+                    st.session_state.fileUpload=uploaded_file.name
+                    handle_pdf_upload(uploaded_file, username, reset=True)
+                else:print("Same file uploaded again")
 
-        with st.form(key="query_form"):
-            query_text = st.text_area("Enter your Query (Type /bye to exit):", height=150)
-            submit_button = st.form_submit_button(label="Submit")
+        st.markdown("""
+                    <div class='title-container'>
+                    <div class='text-container'>
+                        <h1>Conversational bot 💬</h1>
+                    </div>
+                </div>
+""", unsafe_allow_html=True)
+        authenticator.logout(location="sidebar")
 
-        if submit_button:
-            if query_text:
-                if query_text.lower() == "/bye":
-                    st.write("Goodbye!")
-                else:
-                    with st.spinner("Fetching response..."):
-                        response = query_rag(query_text)
-                    st.markdown(f"<div>{response}</div>", unsafe_allow_html=True)
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
 
-        st.markdown("</div>", unsafe_allow_html=True)
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
+        # Accept user input
+        if prompt := st.chat_input("Ask your query"):
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(prompt)
+
+            with st.spinner("Fetching response..."):
+                response = query_rag(prompt, username=username)
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                st.markdown(response)
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
+    elif st.session_state["authentication_status"] == False:
+        st.error('Username/password is incorrect')
+    elif st.session_state["authentication_status"] == None:
+        st.warning('Please enter your username and password')
+
+
+
+    
 if __name__ == "__main__":
     main()
--- populate_database.py
+++ populate_database.py
@@ -1,80 +1,99 @@
+'''
+latest version(25/08/24)
+user sessions working(not interfaring with different users)
+headings are working fine now
+'''
 import os
 import sys
 import io
 import shutil
-import logging
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from get_embedding_function import get_embedding_function
 from langchain_community.vectorstores import Chroma
-from chromadb.config import Settings
+from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
 from pdf2image import convert_from_path
 import pytesseract
 from PIL import Image
 import fitz
 
-# Setup logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Set the tesseract_cmd path
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
 CHROMA_PATH = "chroma"
 DATA_PATH = "PDFs"
 
-def main(reset=False, db=None):
-    logger.info("Populate_database_UI_main function called")
+def main(reset=False, db=None, username = None):
+    print("Populate_database_UI_main function called")
+    print(reset)
     if reset:
-        logger.info("✨ Clearing Database")
+        print("✨ Clearing Database")
         clear_database()
+    else:return
 
     # Ensure Chroma directory exists
-    if not os.path.exists(CHROMA_PATH):
-        os.makedirs(CHROMA_PATH)
-
-    db = Chroma(
-        persist_directory=CHROMA_PATH,
-        embedding_function=get_embedding_function(),
-        client_settings=Settings()
-    )
-
+    if not os.path.exists(CHROMA_PATH+"/"+username):
+        os.makedirs(CHROMA_PATH+"/"+username)
+    # settings = Settings(tenant="default_tenant", database="default_database")
+    # db = Chroma(persist_directory=CHROMA_PATH+"_"+username, embedding_function=get_embedding_function(), settings = settings)
+    db = Chroma(persist_directory=CHROMA_PATH+"_"+username, embedding_function=get_embedding_function())
+    db.persist()
     # Create (or update) the data store.
-    documents = load_documents(DATA_PATH)
-    documents += extract_text_from_images_in_pdf(DATA_PATH)
+    documents = load_documents(DATA_PATH+"/"+username)
+    documents+=extract_text_from_images_in_pdf(DATA_PATH+"/"+username)
     chunks = split_documents(documents)
     add_to_chroma(chunks, db)
 
-def load_documents(data_path):
-    document_loader = PyPDFDirectoryLoader(data_path)
+def load_documents(DATA_PATH):
+    document_loader = PyPDFDirectoryLoader(DATA_PATH)
     return document_loader.load()
 
+# def extract_text_from_images(DATA_PATH):
+#     image_documents = []
+#     for pdf_file in os.listdir(DATA_PATH):
+#         if pdf_file.endswith(".pdf"):
+#             pdf_path = os.path.join(DATA_PATH, pdf_file)
+#             images = convert_from_path(pdf_path)
+#             for i, image in enumerate(images):
+#                 text = pytesseract.image_to_string(image)
+
+                
+#                 if text.strip():  # Only add if there's some text extracted
+#                     print("\n\n\n\n\n"+text+"\n\n\n\n\n")
+#                     metadata = {"source": pdf_file, "page": i+1}
+#                     image_documents.append(Document(page_content=text, metadata=metadata))
+#                     print(image_documents)
+#     return image_documents
+
 def extract_text_from_images_in_pdf(pdf_path):
+    # Open the PDF document
     image_documents = []
     for doc in os.listdir(pdf_path):
-        pdf_document = fitz.open(os.path.join(pdf_path, doc))
+        pdf_document = fitz.open(pdf_path+"/"+doc)
 
+        # Loop through each page
         for page_number in range(len(pdf_document)):
             page = pdf_document.load_page(page_number)
             image_list = page.get_images(full=True)
 
+            # Loop through each image in the page
             for img_index, img in enumerate(image_list):
                 xref = img[0]
                 base_image = pdf_document.extract_image(xref)
                 image_bytes = base_image["image"]
 
+                # Load the image using PIL
                 image = Image.open(io.BytesIO(image_bytes))
-                text = pytesseract.image_to_string(image).strip()
 
-                if text:
-                    metadata = {"source": doc, "page": page_number + 1}
-                    image_documents.append(Document(page_content=text, metadata=metadata))
-                    logger.info(f"Extracted text from {doc}, page {page_number + 1}")
+                # Use Tesseract to extract text from the image
+                text = pytesseract.image_to_string(image).strip()
+                if text.strip():  # Only add if there's some text extracted
+                    # print("\n\n\n\n\n"+text+"\n\n\n\n\n")
+                    metadata = {"source": os.path.basename(pdf_path), "page": page_number}
+                    image_documents.append(Document(page_content=text.strip(), metadata=metadata))
+                    # print(image_documents)
 
     return image_documents
 
-def split_documents(documents):
+def split_documents(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=800,
         chunk_overlap=80,
@@ -82,28 +101,40 @@ def split_documents(documents):
     )
     return text_splitter.split_documents(documents)
 
-def add_to_chroma(chunks, db):
+def add_to_chroma(chunks: list[Document], db):
+    # Calculate Page IDs.
     chunks_with_ids = calculate_chunk_ids(chunks)
 
-    existing_items = db.get(include=[])
+    # Add or Update the documents.
+    existing_items = db.get(include=[])  # IDs are always included by default
     existing_ids = set(existing_items["ids"])
-    logger.info(f"Number of existing documents in DB: {len(existing_ids)}")
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
 
-    new_chunks = [chunk for chunk in chunks_with_ids if chunk.metadata["id"] not in existing_ids]
 
-    if new_chunks:
-        logger.info(f"👉 Adding new documents: {len(new_chunks)}")
+    #comment these two lines if you want the system to retain previous uploaded PDFs data.
+    for doc_id in existing_ids:   
+        db.delete(doc_id)
+
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if len(new_chunks):
+        print(f"👉 Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
 
         total_chunks = len(new_chunks)
         for i, chunk in enumerate(new_chunks, start=1):
             db.add_documents([chunk], ids=[chunk.metadata["id"]])
             progress_percentage = (i / total_chunks) * 100
-            logger.info(f"Progress: {progress_percentage:.2f}% ({i}/{total_chunks} chunks added)")
+            print(f"Progress: {progress_percentage:.2f}% ({i}/{total_chunks} chunks added)", end='\r')
 
+        sys.stdout.flush()
         db.persist()
-        logger.info("✅ All new documents have been added and persisted")
+        print("✅ All new documents have been added and persisted")
     else:
-        logger.info("✅ No new documents to add")
+        print("✅ No new documents to add")
 
 def calculate_chunk_ids(chunks):
     last_page_id = None
@@ -125,10 +156,11 @@ def calculate_chunk_ids(chunks):
 
     return chunks
 
+
+
 def clear_database():
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
-        logger.info("Database cleared")
 
 if __name__ == "__main__":
     main()
